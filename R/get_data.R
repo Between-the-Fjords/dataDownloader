3b22d453098952d784cd3555f4b2b4814bd109e0
richardjtelford <richard.telford@bio.uib.no>
2019-11-27 23:24:57 +0100

    use file hash rather than stored metadata file

--- R/get_data.R
+++ R/get_data.R
@@ -1,43 +1,41 @@
+#' get_data
+#' @param file character, name of file to download
+#' @param path character, output path. Defaults to working directory
+#' @details 
+#' logic
+# !file exists - download
+# file exists & !matching hashes - download
+
 #' @importFrom glue glue
 #' @importFrom fs file_exists
+#' @importFrom digest digest
 #' @export
 
-get_file <- function(
-  file, path  = ".", force = FALSE,
-  osf_metadata_file = ".osf_metadata.RDS"){
-  
-  #make osf_metadata path/file
-  osf_metadata_file <- file.path(path, osf_metadata_file) 
-  #read existing osf_metadata if any
-  osf_metadata <- if(file_exists(osf_metadata_file)){
-    readRDS(osf_metadata_file)
-  } else {
-    message(glue("File {osf_metadata_file} not found. Will attempt to make it."))
-    NULL
-  }
+get_file <- function(file, path  = "."){
+  filepath <- file.path(path, file)
 
   #get osf id of file
   #magic
   
   #get new meta_data
-  new_meta <- osf_retrieve_file(xxx)
+  new_meta <- osf_retrieve_file("xxx")
   
     
   #check local file exists
-  if(file_exists(file.path(data, file))){
+  if(file_exists(filepath)){
     
-  #check hash matches  
-  #if hash matches & !force
-  message(glue("{file} already up to date. \nUse force = TRUE to force new download"))
+    #get hash of local file
+    hash <- digest(file = filepath)
     
-  #else
+    #if hash matches & !force
+    if(identical(hash, newhash)){
+      message(glue("{file} already up to date."))
+      return()
+    }
   }  
-
-  #create metadata
-  #write metadata
+  #download
+  osf_download(new_meta, path = pathfile)
+  
 }
 
-#logic
-#!file exists - download
-#file exists & !matching hashes - download
-#file exists & matching hashes & force = TRUE - download
+
